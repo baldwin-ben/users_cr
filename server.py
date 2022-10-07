b85dce2d75824168e9d2f921061bbf03be24ab1b
Ben Baldwin <baldwin.ben55@gmail.com>
2022-10-06 19:39:03 -0600

    first commit

--- server.py
+++ server.py
@@ -3,21 +3,63 @@ from flask import Flask, render_template, request, redirect
 from user_model import User
 app = Flask(__name__)
 
+# Home page route. Renders home page
+
 @app.route("/")
 def index():
-    # call the get all classmethod to get all users
     users = User.get_all()
     print(users)
     return render_template("read_all.html", users=users) #users has to match what is in jinja in order for your page to load the data.
 
+# route used to render create.html file
 
-@app.route("/user/new")
+@app.route("/users/new")
 def new_user_form():
     return render_template("create.html")
 
-@app.route("/user/create", methods = ['POST'])
+# route that redirects you to the users page once it has been created.
+
+@app.route("/users/create", methods = ['POST'])
 def create_user():
-    User.create_user(request.form)
+    id = User.create_user(request.form)
+    return redirect(f"/users/{id}")
+
+#rendering edit_user form specified in return statement
+
+@app.route("/users/<int:id>/edit")
+def edit_user_id(id):
+    data = {"id":id}
+    user = User.get_one(data)
+    return render_template("edit_user.html", user=user)
+
+# submitting the new user to database. Connected to @classmethod edit.
+# id is specified in url and passed into data dictionary.
+# in the data dictionary we supply what data we want to update.
+@app.route("/users/<int:id>/submit", methods = ['POST'])
+def edit_user(id):
+    data = {'id':id,
+    'first_name': request.form["first_name"],
+    'last_name': request.form["last_name"],
+    'email': request.form["email"]
+    }
+    User.edit(data)
+    return redirect("/")
+
+# route that shows the selected users information.
+# this route is connected to my get one user class.
+
+@app.route("/users/<int:id>")
+def show_user(id):
+    data = {'id':id}
+    this_user = User.get_one(data) # this calls the classmethod get_one from our user model
+    return render_template("read_one.html", user=this_user)
+
+# route that deletes the selected user
+
+@app.route("/users/delete/<int:id>")
+def delete_user(id):
+    data = {'id':id}
+    User.delete_user(data)
     return redirect("/")
 
 if __name__ == "__main__":
--- user_model.py
+++ user_model.py
@@ -1,6 +1,8 @@
 # import the function that will return an instance of a connection
 from mysqlconnection import connectToMySQL
-# model the class after the friend table from our database
+
+# Our class user which takes in the databases table in order. Model it after your table.
+
 class User:
     def __init__( self , data ):
         self.id = data['id']
@@ -10,7 +12,9 @@ class User:
         self.created_at = data['created_at']
         self.updated_at = data['updated_at']
 
-    # Now we use class methods to query our database
+# Now we use class methods to query our database
+# class method that gets all users in our db. This class method is called in our home page.
+
     @classmethod
     def get_all(cls):
         query = "SELECT * FROM users;"
@@ -23,12 +27,37 @@ class User:
             all_users.append( cls(users) )
         return all_users
 
+# class method get_one is linked to the show_user and edit user in my server.py
 
     @classmethod
-    def create_user (cls, data ):
+    def get_one(cls, data):
+        query = "SELECT * FROM users WHERE id = %(id)s"
+        results = connectToMySQL('users_schema').query_db(query,data)
+        if len(results) > 0:
+            return cls(results[0])
+        return False
+
+# edits all the specified users info. Used in my users/int:id/submit method, edit_user.
+
+    @classmethod
+    def edit(cls, data):
+        query = "UPDATE users SET first_name = %(first_name)s, last_name = %(last_name)s, email = %(email)s WHERE id = %(id)s;"
+        return connectToMySQL('users_schema').query_db(query,data)
+
+# query that is used to create user in my create_user method.
+
+    @classmethod
+    def create_user (cls, data):
         query = "INSERT INTO users ( first_name , last_name , email) VALUES ( %(first_name)s , %(last_name)s , %(email)s );"
         # data is a dictionary that will be passed into the save method from server.py
-        return connectToMySQL('users_schema').query_db( query, data )
+        return connectToMySQL('users_schema').query_db(query,data)
+
+# deletes selected user. Used in delete_user method.
+
+    @classmethod
+    def delete_user (cls, data):
+        query = "DELETE FROM users WHERE id = %(id)s;"
+        return connectToMySQL('users_schema').query_db(query,data)
 
 # import the function that will return an instance of a connection
 from mysqlconnection import connectToMySQL
